89192a8c7835d9e322e3b7b5de450bdbb2ef7352
Rodrigo Georgetti <rodrigogeorgetti.cba@gmail.com>
2024-07-22 22:28:23 -0300

    cambios

--- back/config/config.js
+++ back/config/config.js
@@ -1,20 +1,14 @@
 require('dotenv').config();
 
 module.exports = {
-    // username: process.env.DB_USERNAME,
-    // password: process.env.DB_PASSWORD,
-    // database: process.env.DB_NAME,
-    // host: process.env.DB_HOST,
-    // port: process.env.DB_PORT,
-    // dialect: process.env.DB_DIALECT,
-host: "localhost",
-port: 3306,
-databse: "db_justinaio",
-username: "root",
-password: "villamaria",
-dialect: 'mysql',
-TOKEN_KEY: 'N13nC1en4ñ0510s48r4S'
+     username: process.env.DB_USERNAME,
+     password: process.env.DB_PASSWORD,
+     database: process.env.DB_NAME,
+     host: process.env.DB_HOST,
+     port: process.env.DB_PORT,
+     dialect: process.env.DB_DIALECT,
 
-    // //Datos para el token
-    // TOKEN_KEY: process.env.TOKEN_KEY,
+
+     //Datos para el token
+     TOKEN_KEY: process.env.TOKEN_KEY,
 };
